042c041e5aec4cd48e1a32f26ac6549c9d9462ac
Cristian Cuello <cristiancuello10@gmail.com>
2021-07-21 19:01:50 -0300

    socket io organizado en carpetas, iniciado express y separado en carpetas

--- src/index.js
+++ src/index.js
@@ -1,55 +1,38 @@
 const express = require('express')
 const app = express()
-const http = require('http');
-const server = http.createServer(app);
 const path = require("path")
 const port = process.env.PORT || 3000
+//--------------Socket io
+const http = require('http');
+const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const formatMessage = require("./utils/messages")
-const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require("./utils/users")
+const socketFunc = require("./socket.io/socket")
+const onConnection = (socket) => { socketFunc(io, socket) }
+//--------------Socket io
+const mainRoutes = require("./routes/main.routes")
+const exphbs = require('express-handlebars');
+
+
 
 //Static
 app.use(express.static(path.join(__dirname, "public")))
+//View engine
+app.set("views", path.join(__dirname, "./views"))
+app.engine('.hbs', exphbs({
+   defaultLayout: null,
+   extname: ".hbs"
+}));
+//Configuracion para que .hbs sea la extension de los templates
+app.set('view engine', '.hbs');
+
+//Routes
+app.use("", mainRoutes)
 
 //Run when client connects
 io.on("connection", socket => {
-
-   socket.on("joinRoom", ({ username, room }) => {
-      const user = userJoin(socket.id, username, room)
-
-      socket.join(user.room);
-      //Welcome current user
-      socket.emit("message", formatMessage("BOT", `Welcome`))
-      //Broadcast when an user connects
-      socket.broadcast
-         .to(user.room)
-         .emit("message", formatMessage("BOT", `${user.username} has joined the chat`))
-
-      //Run when client disconects
-      socket.on("disconnect", () => {
-         const user = userLeave(socket.id)
-         if (user) {
-            io.to(user.room).emit("message", formatMessage("BOT", `${user.username} has left the chat`))
-         }
-      })
-
-      //Socket escuchando al evento chatMessage
-      socket.on("chatMessage", (msg) => {
-         const user = getCurrentUser(socket.id);
-         console.log(socket.id)
-
-         io.to(user.room)
-            .emit("message", formatMessage(user.username, msg))
-      })
-
-      //Send users in room
-      io.to(user.room).emit("roomUsers", {
-         room: user.room,
-         users: getRoomUsers(user.room)
-      })
-
-   })
+   onConnection(socket)
 })
+
 
 server.listen(port, () => console.log(`Example app listening on port ${port}`))
